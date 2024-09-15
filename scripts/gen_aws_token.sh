5780483faf2934a2c230769ac6b146e6bcb3d9ef
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-09-15 13:09:04 +0530

    Update script to use aws_mfa_arn env variable

--- scripts/gen_aws_token.sh
+++ scripts/gen_aws_token.sh
@@ -2,12 +2,17 @@
 
 echo "mfa_token: $1"
 
-json_string=$(aws sts get-session-token --serial-number <<add-mfa-arn>> --token-code $1)
+if [ -z "$aws_mfa_arn" ]; then
+  echo "aws_mfa_arn env variable is not set"
+  exit 1
+fi
+
+json_string=$(aws sts get-session-token --serial-number $aws_mfa_arn --token-code $1)
 
 echo "$json_string"
 
 # Extract and format params with "export " prefix
-params=$(echo "$json_string" | jq -r '.Credentials | to_entries[] | "export " + .key + "=" + .value')
+params=$(echo "$json_string" | jq -r '.Credentials | to_entries[] | select(.key != "Expiration") | "export " + .key + "=" + .value')
 
 # Transform the key values
 output_string=$(echo "$params" | sed -E 's/export AccessKeyId=/export AWS_ACCESS_KEY_ID=/g; s/export SecretAccessKey=/export AWS_SECRET_ACCESS_KEY=/g; s/export SessionToken=/export AWS_SESSION_TOKEN=/g')
